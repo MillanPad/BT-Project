9b1681072b873c563dc6e20cb2e75ba86fc89c26
MillanPad <73239549+MillanPad@users.noreply.github.com>
2023-04-21 12:25:21 +0200

    Update requests.py
    descomentando

--- nOBEX/requests.py
+++ nOBEX/requests.py
@@ -1,33 +1,9 @@
-#!/usr/bin/env python
 
-"""
-requests.py - Classes encapsulating OBEX requests.
-
-Copyright (C) 2007 David Boddie <david@boddie.org.uk>
-
-This file is part of the nOBEX Python package.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 import struct
 from nOBEX.common import OBEX_Version, Message, MessageHandler
 
 class Request(Message):
-    # Define the additional format information required by requests.
-    # Subclasses should redefine this when required to ensure that their
-    # minimum lengths are calculated correctly.
     format = ""
 
     def __init__(self, data = (), header_data = ()):
