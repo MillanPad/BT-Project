55d7c831d4200a06680386d1daace8e9efc70627
MillanPad <73239549+MillanPad@users.noreply.github.com>
2023-04-21 13:36:54 +0200

    Update sendMsg.py
    Arreglando la llamada al comando AT

--- sendMsg.py
+++ sendMsg.py
@@ -4,5 +4,5 @@ def sendMsg(address,message):
     # Conectamos con el dispositivo Bluetooth
     socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     socket.connect((address, 3))
-    socket.send(f"AT+MAPSND={str(message)}\r\n".encode())
+    socket.send(f"AT+MAPSND={str(address)},{str(message)}\r\n".encode())
     socket.close()
