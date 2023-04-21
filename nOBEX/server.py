73c9a087e056185f8c54ad7050e24c6b29afdaff
MillanPad <73239549+MillanPad@users.noreply.github.com>
2023-04-21 12:24:13 +0200

    Update server.py
    descomentando

--- nOBEX/server.py
+++ nOBEX/server.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-
-"""
-server.py - Server classes for handling OBEX requests and sending responses.
-
-Copyright (C) 2007 David Boddie <david@boddie.org.uk>
-
-This file is part of the nOBEX Python package.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 from nOBEX.common import OBEX_Version
 from nOBEX import bluez_helper
 from nOBEX import headers
@@ -78,8 +55,6 @@ class Server(object):
             return self.max_packet_length
 
     def send_response(self, socket, response, header_list = []):
-        # response encoding will handle making sure we split it
-        # appropriately. we just need to send each chunk
         for h in header_list:
             response.add_header(h)
         chunks = response.encode(self._max_length(), True)
