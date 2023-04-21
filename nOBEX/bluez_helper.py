43d528b508a99eab9d4617ac9a55af81fa59539a
MillanPad <73239549+MillanPad@users.noreply.github.com>
2023-04-21 12:31:03 +0200

    Update bluez_helper.py
    descomentando

--- nOBEX/bluez_helper.py
+++ nOBEX/bluez_helper.py
@@ -1,23 +1,3 @@
-"""
-bluez_helper.py - functions for creating sockets and using SDP
-
-Copyright (C) 2017 Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
-
-This file is part of the nOBEX Python package.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 import atexit, socket, subprocess
 import xml.etree.ElementTree as ET
@@ -44,9 +24,6 @@ def get_available_port(address=BDADDR_ANY):
 
     raise SDPException("All ports are in use!")
 
-# We are using the deprecated Bluez "Service" API instead of the new "Profile"
-# API since the "Service" API works more the way I want it to.
-# sdptool is the easy/lazy way to use this API without native code
 
 adv_services = set()
 
@@ -55,11 +32,8 @@ def stop_all():
         sn = adv_services.pop()
         stop_advertising(sn, False)
 
-# clean up whatever services we started whenever we close the server
 atexit.register(stop_all)
 
-# Python versions older than 3.5 don't have subprocess.run
-# This wrapper produces equivalent functionality on old versions
 def subrun(args, stdout=None):
     if hasattr(subprocess, "run"):
         return subprocess.run(args, stdout=stdout)
@@ -115,7 +89,6 @@ def _search_record(name, bdaddr):
     if val.returncode != 0:
         raise SDPException("sdptool search returned %i" % val.returncode)
 
-    # Strip out lines that are not XML
     xml_lines = []
     for line in val.stdout.splitlines():
         if line.startswith(b'<') or line.startswith(b'\t'):
@@ -126,15 +99,12 @@ def _search_record(name, bdaddr):
     if serv_count < 1:
         raise SDPException("Service %s not found on %s" % (name, bdaddr))
     elif serv_count > 1:
-        # take just the first occurance
         xml_str = xml_str[:xml_str.index(b'</record>') + 9]
 
     try:
         tree = ET.fromstring(xml_str)
     except ET.ParseError:
         raise SDPException("Error parsing XML SDP record")
-
-    # Workaround to HF AG (0x111f) also containing HF (0x111e) class
     if name.upper() == "HF":
         serv_classes = set()
         sc_attr = _find_attr(tree, "0x0001")[0]
@@ -143,7 +113,7 @@ def _search_record(name, bdaddr):
         if 0x111f in serv_classes:
             raise SDPException("HF on %s is HFAG" % bdaddr)
 
-    # this code is probably fragile
+
     handle = _find_attr(tree, "0x0000")[0].attrib["value"]
     channel = int(_find_attr(tree, "0x0004")[0][1][1].attrib["value"], 16)
     return handle, channel
@@ -180,7 +150,7 @@ def list_paired_devices():
     if val.returncode != 0:
         raise SDPException("bluetoothctl paired-devices returned %i" % val.returncode)
 
-    # Extract the MAC addresses
+
     for line in val.stdout.splitlines():
         fields = line.rstrip().split()
         devs.add(fields[1].decode('latin-1'))
